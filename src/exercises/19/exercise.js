b14afbf1bc924a451ce8e99e82635ff606d95d3e
Raziq Ali <62842014+raziqali0306@users.noreply.github.com>
2022-11-17 10:43:10 +0000

    completes exercises 16-20

--- src/exercises/19/exercise.js
+++ src/exercises/19/exercise.js
@@ -1,3 +1,14 @@
 // Comment below what did you understand from the previous 3 exercises about
 // - prototype
 // - __proto__
+
+/**
+ * for every class Prototype is a default entry we have
+ * its like a blueprint of the object
+ * and __proto__ gives the prototype of the prototype of object
+ * we can add methods to prototype as follows
+ * - Book.prototype.getDetails = function() { ... }
+ * when we console.log any object of Book class it has prototype(even if the object is empty i.e {})
+ * and we have this new getDetails function in Book.prototype
+ * These have Object methods like constructor, hasOwnProperty etc... along with getter and setter.
+ */
