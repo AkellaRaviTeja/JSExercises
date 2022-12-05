f03101f15ef6e68600560044d336a38cd2128aee
chaitanya-191855 <raavichaitu4014@gmail.com>
2022-12-05 16:39:33 +0530

    exercises10-15 done

--- src/exercises/13/exercise.js
+++ src/exercises/13/exercise.js
@@ -5,6 +5,13 @@ const employee = {
   lastName: "Teja",
   age: 29,
   id: 1122,
-  getFullName: function () {},
-  checkVoteEligiblity: function () {},
+  getFullName: function () {
+    return this.firstName + " " + this.lastName;
+  },
+  checkVoteEligiblity: function () {
+    if (this.age > 18) {
+      return "Eligible";
+    }
+    return "NotEligible";
+  },
 };
