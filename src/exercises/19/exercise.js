4ec750b5ee54651e7b3c5cc6d6914ddd7ee3b8e0
chaitanya-191855 <raavichaitu4014@gmail.com>
2022-12-06 15:08:27 +0530

    exercise16-20 done

--- src/exercises/19/exercise.js
+++ src/exercises/19/exercise.js
@@ -1,3 +1,13 @@
 // Comment below what did you understand from the previous 3 exercises about
 // - prototype
 // - __proto__
+
+/*
+Prototypes are the mechanism by which JavaScript objects inherit features from one another.
+Prototypes are a powerful and very flexible feature of JavaScript, making it possible to reuse
+code and combine objects.
+Every object in JavaScript has a built-in property, which is called its prototype. The prototype 
+is itself an object, so the prototype will have its own prototype, making what's called a prototype chain. 
+The chain ends when we reach a prototype that has null for its own prototype.
+__proto__ is one of the function of prototype 
+*/
--- src/exercises/20/exercise.js
+++ src/exercises/20/exercise.js
@@ -1,3 +1,16 @@
 // What is debugger;
 // - Comment your understanding of it.
 // - Paste the link of the MDN article here.
+
+/*
+debugging means the process of finding the bugs or error in the code. debugger will help to understand 
+the code line by line,by using this identification of dugs in code is possible.
+we can start debugging the code by placing the keyword - debugger
+The debugger statement invokes any available debugging functionality, such as setting a breakpoint.
+If no debugging functionality is available, this statement has no effect
+
+By using this debugger concept we can understand the line by line execution of the code, and find the place of
+error occuring.
+
+MDN article Link: (https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Statements/debugger)
+*/
