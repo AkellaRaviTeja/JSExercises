428de90804fe3b8e106987279a9f09602a5412d9
chaitanya-191855 <raavichaitu4014@gmail.com>
2022-12-09 15:42:27 +0530

    exercise26-32 done

--- src/exercises/32/exercise.js
+++ src/exercises/32/exercise.js
@@ -32,6 +32,56 @@
     "uct": 330,
     "website": "http://www.hyderabad.aero/"
 }
-
-
 */
+
+class Airport {
+  async getInfoFromIATA(IATAcode) {
+    const options = {
+      method: "GET",
+      headers: {
+        "X-RapidAPI-Key": "00f1f0b8femsh3ac84db90091267p1abf61jsn5a72a6ac83dd",
+        "X-RapidAPI-Host": "airport-info.p.rapidapi.com",
+      },
+    };
+    let response = await fetch(
+      `https://airport-info.p.rapidapi.com/airport?iata=${IATAcode}`,
+      options
+    );
+    return await response.json();
+
+    //   .then((response) => response.json())
+    //   .then((response) => console.log(response))
+    //   .catch((err) => console.error("error"));
+  }
+  async getInfoFromICAO(ICAOCode) {
+    const options = {
+      method: "GET",
+      headers: {
+        "X-RapidAPI-Key": "00f1f0b8femsh3ac84db90091267p1abf61jsn5a72a6ac83dd",
+        "X-RapidAPI-Host": "airport-info.p.rapidapi.com",
+      },
+    };
+
+    let response = await fetch(
+      `https://airport-info.p.rapidapi.com/airport?icao=${ICAOCode}`,
+      options
+    );
+    return await response.json();
+  }
+}
+
+let button = document.querySelector("#enter");
+button.addEventListener("click", async () => {
+  let obj = new Airport();
+  var Iata = document.getElementById("IATAText").value;
+  var Icao = document.getElementById("ICAOText").value;
+  if (Iata === "" && Icao === "") {
+    alert("enter any one text feild");
+    return;
+  }
+  if (Iata) {
+    console.log(obj.getInfoFromIATA(Iata));
+  } else {
+    console.log(obj.getInfoFromICAO(Icao));
+  }
+});
