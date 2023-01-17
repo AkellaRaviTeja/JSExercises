18ef4c52f7cb70ff5ae76d4788e8719dd5ee64e6
Hema Satyanarayana Bokka <hemasatyanarayan113@gmail.com>
2023-01-17 12:48:52 +0530

    JS Exercises 21-25 are completed

--- src/exercises/25/exercise.js
+++ src/exercises/25/exercise.js
@@ -1,1 +1,16 @@
 // Read what is a closure and give an example here
+
+//Closure: Closure is a combination of function and its surrounding state.
+const add = (num) => {
+   const add5 = () => {
+       console.log(num+5)
+   }
+   return add5
+} 
+
+let b=add(4)
+console.log(b())
+
+//In the above example, the  function "add" returns the definition of the function "add5".
+//Here the concept of closures comes into picture, that the "add5" function is bundled with "num" value.
+//So that we can access the num value outside the "add" function.
