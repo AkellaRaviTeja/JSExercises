bfec3dbddef24a639d2dcc465d61718d1dbf2e5d
Hema Satyanarayana Bokka <hemasatyanarayan113@gmail.com>
2023-01-18 15:18:27 +0530

    JS Exercises 26-30 are completed.

--- src/exercises/26/exercise.js
+++ src/exercises/26/exercise.js
@@ -7,4 +7,28 @@
 
 const counter = (function () {
   //Your code goes here.
+
+  let val;
+  const value = i => val = i;
+  const increment = () => val++;
+  const decrement = () => val--;
+  
+
+  // value(35);
+  // increment();
+  // console.log(val)
+
 })();
+
+// Yes, there is a anonymus function used here. The function which is assigned to counter variable is
+// a anonymus function.
+
+// IIFE stands for Immediately Invoked Function Expression. IIFE is a javascript function that runs as
+// soon as it is defined. We have used one here.
+
+// Revealing Module Pattern is JavaScript’s design pattern that is available for users to actually
+// organize JavaScript’s codes in modules which actually gives better code structure and helps in
+//  making things either private or public to users.
+
+// Basically, a function which takes another function as an argument or 
+// returns a function is known as a higher order function.
--- src/exercises/29/exercise.js
+++ src/exercises/29/exercise.js
@@ -4,3 +4,14 @@
 function calculateVolume(length, breadth, height) {
   return length * breadth * height;
 }
+
+function calculateVolume(length) {
+  return function (breadth) {
+    return function (height) {
+      return length*breadth*height
+    }
+  }
+}
+
+// Currying is a transformation of functions that translates a function from callable as f(a, b, c) 
+// into callable as f(a)(b)(c). Currying doesn’t call a function. It just transforms it.
