64bdd9df7c405d56afa5e9f95eddb9e3ff36197e
yashwanthreddy-909 <yaswanthreddy909@gmail.com>
2023-01-20 13:20:41 +0530

    Exerice-7 done

--- src/exercises/7/exercise.js
+++ src/exercises/7/exercise.js
@@ -4,4 +4,7 @@
 function commaSeparatedString(inputArray) {
   // Given an Array like ["firstName","lastName"]
   // Return a comma separated String like "firstName,lastName"
+  return `${inputArray[0]},${inputArray[1]}`
 }
+
+// console.log(commaSeparatedString(['Yash','Reddy']))
