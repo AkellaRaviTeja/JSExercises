dbe7617ba34a77488432cf8f69a27126bbcb40df
yashwanthreddy-909 <yaswanthreddy909@gmail.com>
2023-01-20 12:02:44 +0530

    Exercise-1 done

--- src/exercises/1/exercise.js
+++ src/exercises/1/exercise.js
@@ -3,7 +3,7 @@
 //  - Fix the issue.
 
 function printOneToTen() {
-  for (const number = 1; number <= 10; number++) {
+  for (let number = 1; number <= 10; number++) {
     console.log(`\n${number}`);
   }
 }
