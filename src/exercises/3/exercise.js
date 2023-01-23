9aa46d5397faf002a139369d1003bf9f90f5f3c5
VasireddyGanesh <vasireddi.ganesh.8@gmail.com>
2023-01-23 11:22:01 +0530

    exercise-3 done

--- src/exercises/3/exercise.js
+++ src/exercises/3/exercise.js
@@ -1,10 +1,10 @@
 // Complete the below function. Find if an input is array or not.
 //  - Comment what is the operator that you have used.
 
-function checkIfArray(input) {
-  let isInputAnArray = false;
+// instanceof Operator is used to check if the input is array or not .
 
-  //...
+function checkIfArray(input) {
+  let isInputAnArray = input instanceof Array;
 
   if (isInputAnArray) {
     console.log("Given input is an Array");
