2ac513a226af4802be2869b1d43c73bd25c41b3c
VasireddyGanesh <vasireddi.ganesh.8@gmail.com>
2023-01-23 12:30:46 +0530

    exercise-9 done

--- src/exercises/9/exercise.js
+++ src/exercises/9/exercise.js
@@ -2,6 +2,12 @@
 // - Use Array methods.
 // - Do not use loops
 
+function multipleBytwo(num){
+  return num*2;
+}
+
 function multiplyArrayByTwo(inputArray) {
   console.log(`Given input array is ${inputArray}`);
+  let resultArray=inputArray.map(multipleBytwo)
+  console.log(`Result array is ${resultArray}`);
 }
