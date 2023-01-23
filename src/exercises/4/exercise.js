04c8045a81b54c4f7b0977bd24006f236e71a78a
VasireddyGanesh <vasireddi.ganesh.8@gmail.com>
2023-01-23 12:09:49 +0530

    exercise-4 done

--- src/exercises/4/exercise.js
+++ src/exercises/4/exercise.js
@@ -2,8 +2,12 @@
 //  - Use string template literal
 //  - Comment its uses.
 
+// Uses : 
+// 1. Template literals are used to insert variables and expressions into Strings without Concatenation
+// 2. Template literals are used to write multiline strings without concatenation
+
 function greeting(firstName, lastName) {
-  let welcomeGreeting;
+  let welcomeGreeting=`Welcome ${firstName} ${lastName}`;
 
   console.log(welcomeGreeting);
 }
