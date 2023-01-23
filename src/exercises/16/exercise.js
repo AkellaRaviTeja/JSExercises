3b38db2fd58030eecd91ee0817d519a117419d94
VasireddyGanesh <vasireddi.ganesh.8@gmail.com>
2023-01-23 15:37:31 +0530

    exercise-16 done

--- src/exercises/16/exercise.js
+++ src/exercises/16/exercise.js
@@ -3,5 +3,27 @@
 // - Create function for all the possible functionalities.
 
 class Leave {
-  //...
+  constructor(leaveAppliedDate,dateOfLeave,reasonForLeave){
+    this.leaveAppliedDate=leaveAppliedDate;
+    this.dateOfLeave=dateOfLeave;
+    this.reasonForLeave=reasonForLeave;
+  }
+  getleaveAppliedDate(){
+    return this.leaveAppliedDate;
+  }
+  getdateOfLeave(){
+    return this.dateOfLeave;
+  }
+  getReasonForLeave(){
+    return this.reasonForLeave;
+  }
+  setleaveAppliedDate(leaveAppliedDate){
+    this.leaveAppliedDate=leaveAppliedDate;
+  }
+  setdateOfLeave(dateOfLeave){
+    this.dateOfLeave=dateOfLeave;
+  }
+  setReasonForLeave(reasonForLeave){
+    this.reasonForLeave=reasonForLeave;
+  }
 }
