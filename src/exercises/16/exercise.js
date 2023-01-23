679a172119e9c8b4c34159cffa484f49c5a5397b
yashwanthreddy-909 <yaswanthreddy909@gmail.com>
2023-01-23 12:30:37 +0530

    Exercise-16 done

--- src/exercises/16/exercise.js
+++ src/exercises/16/exercise.js
@@ -4,4 +4,13 @@
 
 class Leave {
   //...
+  constructor(days) {
+    this.days = days
+  }
+  getDaysCount() {
+    return this.days
+  }
 }
+
+let leave = new Leave(10);
+console.log(leave.getDaysCount())
