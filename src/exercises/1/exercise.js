6bf71d68357f678130b82efb9c06d1eff57ee538
VasireddyGanesh <vasireddi.ganesh.8@gmail.com>
2023-01-23 10:46:35 +0530

    printOneToTen done

--- src/exercises/1/exercise.js
+++ src/exercises/1/exercise.js
@@ -2,8 +2,10 @@
 //  - comment the reason for failure.
 //  - Fix the issue.
 
+// Reason : Value of an Constant Variable Cannot be modified
+
 function printOneToTen() {
-  for (const number = 1; number <= 10; number++) {
+  for (let number = 1; number <= 10; number++) {
     console.log(`\n${number}`);
   }
 }
