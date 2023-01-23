7b9934599ece9fee1e1438a05bbb8f6ce10ce172
yashwanthreddy-909 <yaswanthreddy909@gmail.com>
2023-01-23 12:55:51 +0530

    Exercise-17 done

--- src/exercises/17/exercise.js
+++ src/exercises/17/exercise.js
@@ -1,1 +1,23 @@
 // Extend the Leave class, to create different classes for different Leave types.
+
+class Leave {
+    //...
+    constructor(days) {
+      this.days = days
+    }
+    getDaysCount() {
+      return this.days
+    }    
+}
+
+class SickLeave extends Leave {
+    constructor(days) {
+        super(days)
+    }
+    getLeaveDetails() {
+        return `this is a Sick leave for ${this.getDaysCount()} days`
+    }
+}
+
+const sickLeave = new SickLeave(10)
+console.log(sickLeave.getLeaveDetails())
