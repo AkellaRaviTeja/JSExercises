78d34c29aae16b67a7b6cf7730820d35b0272d69
VasireddyGanesh <vasireddi.ganesh.8@gmail.com>
2023-01-23 13:38:06 +0530

    exercise-14 done

--- src/exercises/14/exercise.js
+++ src/exercises/14/exercise.js
@@ -1,6 +1,10 @@
 // Get the full name of the new employee. See how you can execute the function "getFullName" on newEmployee object.
 // - Comment your findings
 
+/* 
+  __proto__ provides a way to inherit properties and methods of another object , Here newEmployee inherits employee Object.Hence newEmployee object can use getFullname method of employee object
+*/
+
 const employee = {
   firstName: "Ravi",
   lastName: "Teja",
@@ -8,10 +12,12 @@ const employee = {
   id: 1122,
   getFullName: function () {
     //Copy the solution from the exercise before this.
+    return `${this.firstName} ${this.lastName}`;
   },
 };
 
 const newEmployee = {
   firstName: "New",
   lastName: "Employee",
+  __proto__: employee
 };
