6062ab2536d19971705bafaec8b9b6e591614bd8
VasireddyGanesh <vasireddi.ganesh.8@gmail.com>
2023-01-24 16:39:51 +0530

    exercise-25 done

--- src/exercises/25/exercise.js
+++ src/exercises/25/exercise.js
@@ -1,1 +1,12 @@
 // Read what is a closure and give an example here
+
+// A closure is the combination of a function bundled together (enclosed) with references to its surrounding state (the lexical environment). 
+// In other words, a closure gives you access to an outer function's scope from an inner function.
+
+function counter(){
+    let count=0;
+    function updateCounter(){
+        return ++count;
+    }
+    return updateCounter;
+}
