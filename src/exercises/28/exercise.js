1cde14f0e134a0ac752abddb5a3f1d7cc61837ec
VasireddyGanesh <vasireddi.ganesh.8@gmail.com>
2023-01-25 10:36:46 +0530

    exercise-28 done

--- src/exercises/28/exercise.js
+++ src/exercises/28/exercise.js
@@ -1,7 +1,11 @@
 // Execute the below function.
 // - There is an issue with the order of messages
 // - Reason here why is it so.
+// - whenever the execution reaches setTimeout() then it will be sent to Browser API and waits for the time to complete meanwhile the execution continues to execute other parts of the code
+
 // - Explain in your own terms what are event loops?
+// - The event loop is a constantly running process that monitors both the callback queue and the call stack.If the call stack is not empty, 
+//   the event loop waits until it is empty and places the next function from the callback queue to the call stack.
 
 function learnEventLoops() {
   console.log("Learning event loops");
@@ -13,4 +17,4 @@ function learnEventLoops() {
   }, 1); // 1 millisec
   console.log("Learning completed!!");
 }
-main();
+learnEventLoops();
