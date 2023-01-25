daab1a77ef840a3dc42350b00981663fcfa6ed80
VasireddyGanesh <vasireddi.ganesh.8@gmail.com>
2023-01-25 10:51:16 +0530

    exercise-29 done

--- src/exercises/29/exercise.js
+++ src/exercises/29/exercise.js
@@ -1,6 +1,12 @@
 // - Explain currying.
+// - Currying is a technique in functional programming that performs the transformation of a function with multiple arguments into several functions containing a single argument in a sequence.
+// - Currying is a transformation of functions that translates a function from callable as f(a, b, c) into callable as f(a)(b)(c).
 // - Convert the below function to curry function
 
-function calculateVolume(length, breadth, height) {
-  return length * breadth * height;
+function calculateVolume(length) {
+  return function(breadth){
+    return function(height){
+      return length*breadth*height;
+    }
+  };
 }
