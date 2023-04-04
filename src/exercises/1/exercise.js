5153f046c34bfa79e60cf5bb77788b0a00c6432f
Gautham-Apxor <gautham.gokulakonda@apxor.com>
2023-04-04 13:40:34 +0530

    exercise-1 done

--- src/exercises/1/exercise.js
+++ src/exercises/1/exercise.js
@@ -3,7 +3,8 @@
 //  - Fix the issue.
 
 function printOneToTen() {
-  for (const number = 1; number <= 10; number++) {
+  for (let number = 1; number <= 10; number++) {
+    // Variables defined with const cannot be Reassigned
     console.log(`\n${number}`);
   }
 }
