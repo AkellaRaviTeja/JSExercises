ddc9c9949818f0f0a42fdb331ea4a6c153f915e0
agodse21 <agodse9@gmail.com>
2023-04-04 18:51:31 +0530

    second exercise complated

--- src/exercises/1/exercise.js
+++ src/exercises/1/exercise.js
@@ -2,8 +2,10 @@
 //  - comment the reason for failure.
 //  - Fix the issue.
 
-// ans: In for loop you use const variable and const is constant we can't
-// change thier value so i have changed const to let
+// ans: In for loop you use const variable and const is constant we can't change thier value so i have changed const to let.
+// Variables defined with const cannot be Redeclared.
+// Variables defined with const cannot be Reassigned.
+// Variables defined with const have Block Scope.
 
 function printOneToTen() {
   for (let number = 1; number <= 10; number++) {
