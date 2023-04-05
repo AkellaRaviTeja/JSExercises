7317309bc245adc8a3513a1d3e856cafd1672241
agodse21 <agodse9@gmail.com>
2023-04-05 10:19:48 +0530

    Third Exercise complated

--- src/exercises/3/exercise.js
+++ src/exercises/3/exercise.js
@@ -1,10 +1,14 @@
 // Complete the below function. Find if an input is array or not.
 //  - Comment what is the operator that you have used.
 
+//ans : I used Array.isArray() , Array.isArray() method checks whether the passed argument is an array or not.
 function checkIfArray(input) {
   let isInputAnArray = false;
 
   //...
+  if (Array.isArray(input)) {
+    isInputAnArray = true;
+  }
 
   if (isInputAnArray) {
     console.log("Given input is an Array");
