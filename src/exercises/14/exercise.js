0f442589cf2cb8637c167b407c76449f6d9712f4
agodse21 <agodse9@gmail.com>
2023-04-05 11:40:41 +0530

    14th exercise complated

--- src/exercises/14/exercise.js
+++ src/exercises/14/exercise.js
@@ -1,6 +1,8 @@
 // Get the full name of the new employee. See how you can execute the function "getFullName" on newEmployee object.
 // - Comment your findings
 
+//Ans: In the example, when we call employee.getFullName, the value of this will be bound to object newEmployee, even when getFullName is not a method of newEmployee.
+
 const employee = {
   firstName: "Ravi",
   lastName: "Teja",
@@ -8,6 +10,7 @@ const employee = {
   id: 1122,
   getFullName: function () {
     //Copy the solution from the exercise before this.
+    return `${this.firstName} ${this.lastName}`;
   },
 };
 
@@ -15,3 +18,5 @@ const newEmployee = {
   firstName: "New",
   lastName: "Employee",
 };
+const fullName = employee.getFullName.call(newEmployee);
+console.log(fullName);
