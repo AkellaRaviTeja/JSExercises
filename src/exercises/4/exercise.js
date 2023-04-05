c090afc330b92a276c09bec735ace1621b3a53bf
agodse21 <agodse9@gmail.com>
2023-04-05 10:24:48 +0530

    Fourth exercise complated

--- src/exercises/4/exercise.js
+++ src/exercises/4/exercise.js
@@ -1,9 +1,10 @@
 // Complete the below function. Print welcome greetings to a person.
 //  - Use string template literal
 //  - Comment its uses.
-
+// ans: Template literals allow you to use strings or embedded expressions in the form of a string
 function greeting(firstName, lastName) {
-  let welcomeGreeting;
+ 
+  let welcomeGreeting = `Welcome, ${firstName} ${lastName}!`;
 
   console.log(welcomeGreeting);
 }
