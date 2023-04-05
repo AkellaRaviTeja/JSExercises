731218dfdac1aa3ff24f154e90b836d0eb582194
agodse21 <agodse9@gmail.com>
2023-04-05 17:19:34 +0530

    28th exercise complated

--- src/exercises/28/exercise.js
+++ src/exercises/28/exercise.js
@@ -1,7 +1,10 @@
 // Execute the below function.
 // - There is an issue with the order of messages
 // - Reason here why is it so.
+//Ans: In the code, the message "Learning completed!!" is printed before the messages printed inside the setTimeout functions, even though the setTimeout functions have less delay times. This is because setTimeout functions are added to the event queue and executed after the main thread has finished executing the synchronous code.
+
 // - Explain in your own terms what are event loops?
+// Ans: An event loop is something that pulls stuff out of the queue and places it into the function execution stack whenever the function stack becomes empty.The event loop is the secret by which JavaScript gives us an illusion of being multithreaded even though it is single-threaded,Event loops are a fundamental part of how JavaScript works. They manage the execution of code by continuously monitoring the call stack and the event queue. The call stack is where synchronous code execution happens, while the event queue is where asynchronous code execution is scheduled. The event loop constantly checks if the call stack is empty and if so, it dequeues the next function from the event queue and executes it.
 
 function learnEventLoops() {
   console.log("Learning event loops");
@@ -13,4 +16,3 @@ function learnEventLoops() {
   }, 1); // 1 millisec
   console.log("Learning completed!!");
 }
-main();
