1737a2186e475b07e838641468bbc2e40f560841
agodse21 <agodse9@gmail.com>
2023-04-05 10:52:58 +0530

    9th exercise complated

--- src/exercises/9/exercise.js
+++ src/exercises/9/exercise.js
@@ -3,5 +3,7 @@
 // - Do not use loops
 
 function multiplyArrayByTwo(inputArray) {
+  let multipliedArray = inputArray.map(number => number * 2); 
   console.log(`Given input array is ${inputArray}`);
+  console.log(`Given input array multiplied by 2 is ${multipliedArray}`);
 }
