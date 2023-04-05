361b1e0c8d20e17584726335f15f6614a215ca53
agodse21 <agodse9@gmail.com>
2023-04-05 13:02:22 +0530

    20th exercise complated

--- src/exercises/20/exercise.js
+++ src/exercises/20/exercise.js
@@ -1,3 +1,8 @@
 // What is debugger;
 // - Comment your understanding of it.
 // - Paste the link of the MDN article here.
+
+// Ans : debugger is a tool in JavaScript that allows developers to pause the execution of code at a specific line and inspect the state of variables and expressions at that point in the code. It is a keyword that can be used in the code, and when the browser encounters it, it will pause the execution and open the browser's developer tools. This allows the developer to step through the code, inspect the values of variables,and any issues that may be present. The debugger statement is commonly used during the development and testing phase to help identify and fix issues in the code.
+
+
+//MDN Link:  https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Statements/debugger
