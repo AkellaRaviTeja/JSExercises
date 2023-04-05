624b13c350d6d04c8c0bcddce1f3d8b9b259edcb
agodse21 <agodse9@gmail.com>
2023-04-05 11:10:02 +0530

    11th exercise complated

--- src/exercises/11/exercise.js
+++ src/exercises/11/exercise.js
@@ -2,4 +2,9 @@
 // - Use Array methods
 // - Do not use loops
 
-function sumOfArray(inputArray) {}
+//Ans: Used reduce() array method to sum of all the elements in the array
+
+function sumOfArray(inputArray) {
+  let sum = inputArray.reduce((acc, currentVal) => acc + currentVal, 0); 
+  console.log(`Sum of the elements in the given array is ${sum}`);
+}
