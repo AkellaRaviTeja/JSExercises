c1a2cd183f1288b2042ec88cde55af0e75472d82
agodse21 <agodse9@gmail.com>
2023-04-05 17:09:40 +0530

    27th exercise complated

--- src/exercises/27/exercise.js
+++ src/exercises/27/exercise.js
@@ -1,8 +1,10 @@
 // What is a callback?
+// Ans:  a callback is a function passed as an argument to another function that is executed after a particular event has occurred or a task has been completed. The function receiving the callback can then call that function to execute the code it contains, thereby allowing the function receiving the callback to be customized with different functionality.
 
-function welcome(name) {
+function welcome(name, callback) {
   setTimeout(() => {
     console.log(`Welcome ${name}`);
+    callback();
   }, 1000);
 }
 
@@ -10,8 +12,7 @@ function success() {
   console.log("Greetings successful!!");
 }
 
-welcome();
-success();
+welcome("Amol", success);
 
 // In the above example:
 // - Before welcoming, success message is printed.
