c637a9b0a022292f4e4de1911d5df3a22af74e8b
agodse21 <agodse9@gmail.com>
2023-04-05 10:33:04 +0530

    Fifth exercise complated

--- src/exercises/5/exercise.js
+++ src/exercises/5/exercise.js
@@ -2,8 +2,11 @@
 // - Find the easiest way to do it.
 // - Comment the function that you used.
 
+
+//ans : I used parseInt function .The parseInt() function parses a string argument and returns an integer of the specified radix.
+
 function binaryToDecimal(binaryString) {
-  let decimalValue;
+  let decimalValue = parseInt(binaryString, 2);
   console.log(
     `Decimal for the given binary string ${binaryString} is ${decimalValue}`
   );
