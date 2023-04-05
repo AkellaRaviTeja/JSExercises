3e77888ff7a4a1c4c99b741bfff9897b59f6f6fe
agodse21 <agodse9@gmail.com>
2023-04-05 10:43:40 +0530

    Seventh exercise complated

--- src/exercises/7/exercise.js
+++ src/exercises/7/exercise.js
@@ -1,7 +1,10 @@
 // Complete the below function.
 // - Use Array methods
 
+// Ans : The join() method returns a new string by concatenating all of the elements in an array, separated by a specified separator.
+
 function commaSeparatedString(inputArray) {
   // Given an Array like ["firstName","lastName"]
   // Return a comma separated String like "firstName,lastName"
+  return inputArray.join(",")
 }
