72d0cf50124de3e3fd6f2b91440b2eb42b00d65e
agodse21 <agodse9@gmail.com>
2023-04-05 14:49:34 +0530

    22th exercise complated

--- src/exercises/22/exercise.js
+++ src/exercises/22/exercise.js
@@ -2,12 +2,17 @@
 // - Use debugging techniques to find the values of x,y
 // - What is "Watch" in chrome debugging console. Share a screenshot of the watch variables at each step.
 
+//Watch in chrome: "Watch" is a feature in the Chrome debugging console that allows developers to monitor the values of specific variables or expressions over time, even as the code continues to execute. It allows developers to keep track of variables and see their values change in real-time. By adding variables or expressions to the watch list, developers can monitor their values, set breakpoints and pause execution when necessary to debug issues.
+
+
 function randomCalculator() {
   let x = 10,
     y = 0;
-  x = x * 10 + 1 - (2 / 2) * 3;
-  y = x - 1 + 3 * 10 - 2;
-  x = x * 2 - (1 / 3) * 3;
+  x = x * 10 + 1 - (2 / 2) * 3; //x=98,y=0
+  y = x - 1 + 3 * 10 - 2; //y=125,x=98
+  x = x * 2 - (1 / 3) * 3; //y=125,x=195
   x = x + 10 - 2 - 3 * 2;
+  // debugger;
   console.log(x);
+  x = 195;
 }
