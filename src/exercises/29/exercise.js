18350dfbbde76e5a07813cca1b38a3bfd841a370
Gautham-Apxor <gautham.gokulakonda@apxor.com>
2023-04-06 16:14:10 +0530

    exercise-29 done

--- src/exercises/29/exercise.js
+++ src/exercises/29/exercise.js
@@ -1,6 +1,14 @@
 // - Explain currying.
+// currying is a technique of evaluating function with multiple arguments, into sequence of functions with single argument. 
+
+
+
 // - Convert the below function to curry function
 
-function calculateVolume(length, breadth, height) {
-  return length * breadth * height;
+function calculateVolume(length) {
+  return function (breadth) {
+    return function (height) {
+      return length * breadth * height;
+    };
+  }
 }
